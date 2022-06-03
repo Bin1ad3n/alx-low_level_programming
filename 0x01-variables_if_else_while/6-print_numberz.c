dc835b8854e565365ff10f027908327485d57888
Osama Mohammed Abduselam <osamagentle@gmail.com>
2022-06-03 10:26:51 -0700

    Mandatory Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all numbers of base 10 starting from 0 without using char
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+		putchar(i + '0');
+		i++
+
+	putchar('\n');
+
+	return (0);
+}
