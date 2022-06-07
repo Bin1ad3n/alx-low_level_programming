8e7ef972765baddffa8aff1f4c365f458ae73242
Osama Mohammed Abduselam <osamagentle@gmail.com>
2022-06-07 04:17:17 -0700

    Mandatory Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of a number
+ * @c: Random number
+ * Return: 0
+ */
+
+int print_last_digit(int n);
+{
+	int l;
+
+	if (n < 0)
+	{
+		l = -1 * (n % 10);
+		_putchar(l + '0');
+		return (l);
+	}
+	else
+	{
+		l = n % 10;
+		_putchar(l + '0');
+		return (l);
+	}
+}
