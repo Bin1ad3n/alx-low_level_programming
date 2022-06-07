acb1ce05a385cc40703eb6b9a5fb3ccf3a8b30b4
Osama Mohammed Abduselam <osamagentle@gmail.com>
2022-06-07 03:51:11 -0700

    Mandatory Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Absolute value
+ * @n: Random integer
+ * Return: 0
+ */
+
+int _abs(int)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
