76739acbfbd86333751c58a249e9af1a6ac87f24
Osama Mohammed Abduselam <osamagentle@gmail.com>
2022-06-07 03:44:46 -0700

    Mandatory Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - Shows whether it is positive, 0 or negative
+ * @c: Random number
+ * Return: 0
+ */
+
+int print_sign(int n)
+{
+	if (c > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (c < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
