89788bb2adc7da5eaff1b681432b0fc30e271f69
Osama Mohammed Abduselam <osamagentle@gmail.com>
2022-06-07 04:32:32 -0700

    Mandatory Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Day
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int d = 0;
+	int om = 0, tm = 0;
+	int oh = 0, th = 0;
+
+	while (d < 1440)
+	{
+		_putchar(th + '0');
+		_putchar(oh + '0');
+		_putchar(':');
+		_putchar(tm + '0');
+		_putchar(om + '0');
+		_putchar('\n');
+		
+		om++;
+
+		if (om > 9)
+		{
+			om = 0;
+			tm++;
+		}
+
+		if (tm > 5)
+		{
+			tm = 0;
+			oh++;
+		}
+
+		if (oh > 9)
+		{
+			oh = 0;
+			th++;
+		}
+
+		d++;
+	}
+}
