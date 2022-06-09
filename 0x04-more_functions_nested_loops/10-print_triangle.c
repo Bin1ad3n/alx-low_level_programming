51d12fa8fa7d37128ba86c12a74d27f53c72dfec
Osama Mohammed Abduselam <osamagentle@gmail.com>
2022-06-09 08:28:00 -0700

    Mandatory Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_triangle - Print a triangle
+ * @size: Size of triangle
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	c = 0;
+
+	i = size - 1;
+	while (c < size)
+	{
+		i = size - 1 - c;
+		j = c + 1;
+		while (i > 0)
+		{
+			_putchar(' ');
+			i--;
+		}
+		while (j > 0)
+		{
+			_putchar('#');
+			j--;
+		}
+		_putchar('\n');
+		c++;
+	}
+
+	if (size <= 0)
+		_putchar('\n');
+}
