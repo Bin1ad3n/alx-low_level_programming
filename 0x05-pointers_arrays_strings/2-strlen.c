3f70e88d1d64e1fa9f52b22004aedc75d18e355b
Osama Mohammed Abduselam <osamagentle@gmail.com>
2022-06-13 11:33:59 -0700

    Mandatory Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Length of string
+ * @s: Pointer
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	
+	return (i);
+}
