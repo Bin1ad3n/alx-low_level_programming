9753048be6d75d3d886f2ba71992691732ac4715
Osama Mohammed Abduselam <osamagentle@gmail.com>
2022-06-15 03:34:16 -0700

    Mandatory Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strncat - Concatenates two strings
+ * @dest: character pointer variable
+ * @src: character pointer variable
+ * @n: integer variable
+ * Return: pointer to dest variable
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x, y = 0;
+
+	for (x = 0; dest[x] != '\0'; x++)
+		;
+	while (src[y] != '\0' && y < n)
+	{
+		dest[x + y] = src[y];
+		y++;
+	}
+
+	return (dest);
+}
