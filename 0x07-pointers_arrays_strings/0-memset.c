db984ff57cb8724c5e5a53e48a791d056e7bdb7d
Osama Mohammed Abduselam <osamagentle@gmail.com>
2022-06-27 00:28:44 -0700

    Mandatory Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,17 @@
 #include "main.h"
-
 /**
- * *_memset - Fill memory
- * @*s: Pointer
- * @b: Character
- * @n: Integer
- * Return: 0
+ * _memset - fill memory with a constant byte
+ * @s:char
+ * @b:char
+ * @n:unsigned int
+ * Return:char
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-	
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
